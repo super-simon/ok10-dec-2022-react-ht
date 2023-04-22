f661903890f3b0ec20ebb70f108dd33e7d0ca035
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2023-04-22 23:18:48 +0300

    add comment button

--- src/components/UserPost/UserPost.tsx
+++ src/components/UserPost/UserPost.tsx
@@ -4,13 +4,18 @@ import "./UserPost.css";
 export default function UserPost({ post }: { post: postInterface }) {
   return (
     <>
-      <img
-        className="user-post__img"
-        src={`https://picsum.photos/id/${10 + post.id}/300`}
-        alt={post.title}
-      />
-      <span className="user-post__title">{post.title}</span>
-      <span className="user-post__body">{post.body}</span>
+      <div className="user-post__card">
+        <div>
+          <img
+            className="user-post__img"
+            src={`https://picsum.photos/id/${10 + post.id}/300`}
+            alt={post.title}
+          />
+          <span className="user-post__title">{post.title}</span>
+          <span className="user-post__body">{post.body}</span>
+        </div>
+        <button>Add Comment</button>
+      </div>
     </>
   );
 }
